a4331311ce298f947b36d85e74b0075ee76d4bc2
nahidplanet <nahidplanet.bd@gmail.com>
2022-05-29 22:13:38 +0600

    final check

--- index.js
+++ index.js
@@ -177,17 +177,17 @@ async function run() {
     })
 
     // make admin api
-    // app.put('/user/admin/:email', verifyJwt, verifyAdmin, async(req, res)=>{
-    //   const email = req.params.email;
-    //   const filter = {email: email};
-    //   const updatedDoc = {
-    //     $set: {
-    //       role: "admin"
-    //     },
-    //   };
-    //   const result = await usersCollection.updateOne(filter, updatedDoc);
-    //   res.send(result);
-    // })
+    app.put('/user/admin/:email', verifyJwt, verifyAdmin, async(req, res)=>{
+      const email = req.params.email;
+      const filter = {email: email};
+      const updatedDoc = {
+        $set: {
+          role: "admin"
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
 
     // useAdmin hooks api
     app.get("/admin/:email", verifyJwt, async(req, res)=>{
